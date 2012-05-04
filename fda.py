7fd5d65d3c362403f21a40b833cb321cad97c61d
Derek Hurley <dzhurley@gmail.com>
2012-05-04 11:20:25 -0700

    refactored a bit, working on cleaner detection

--- fda.py
+++ fda.py
@@ -15,55 +15,36 @@ facecade = cv.Load("data/haarcascade_frontalface_default.xml")
 eyecade = cv.Load("data/haarcascade_eye.xml")
 mouthcade = cv.Load("data/haarcascade_mcs_mouth.xml")
 
+# consistent HaarDetectObjects parameters
+scale = 1.2
+flags = cv.CV_HAAR_DO_CANNY_PRUNING
+neighbors = 2
+
 # storage for opencv work
 storage = cv.CreateMemStorage()
 
 
-def detect(frame):
-    """detect the facial features and draw rectangles around matches
+def detect_feature(frame, cade, color=255):
+    """detect a single feature and draw a box around the matching area
 
     By using Canny Pruning, we disregard certain image regions to boost the
-    detection performance. Note that face detection is only supported. Tweaking
-    the parameters passed to HaarDetectObjects greatly affects performance.
+    performance. Tweaking the parameters passed to HaarDetectObjects greatly
+    affects performance as well.
     """
-    # detect the entire face
-    fobjs = cv.HaarDetectObjects(frame,
-                                 facecade,
-                                 storage,
-                                 scale_factor=1.2,
-                                 min_neighbors=2,
-                                 flags=cv.CV_HAAR_DO_CANNY_PRUNING,
-                                 min_size=(80, 80))
-    # draw facial rectangle
-    for (x, y, w, h), n in fobjs:
-        blue = cv.Scalar(0, 0, 255)
-        cv.Rectangle(frame, (x, y), (x + w, y + h), blue)
+    # detect feature in image
+    objs = cv.HaarDetectObjects(frame, cade, storage, scale_factor=scale,
+                                min_neighbors=neighbors, flags=flags,
+                                min_size=(80, 80))
+    # draw rectangle around matched feature
+    for (x, y, w, h), n in objs:
+        cv.Rectangle(frame, (x, y), (x + w, y + h), color)
 
-    # detect eyes
-    eobjs = cv.HaarDetectObjects(frame,
-                                 eyecade,
-                                 storage,
-                                 scale_factor=1.2,
-                                 min_neighbors=2,
-                                 flags=cv.CV_HAAR_DO_CANNY_PRUNING,
-                                 min_size=(30, 30))
-    # draw rectangle around eyes
-    for (x, y, w, h), n in eobjs:
-        red = cv.Scalar(255, 0, 0)
-        cv.Rectangle(frame, (x, y), (x + w, y + h), red)
 
-    # detect mouth
-    mobjs = cv.HaarDetectObjects(frame,
-                                 mouthcade,
-                                 storage,
-                                 scale_factor=1.2,
-                                 min_neighbors=2,
-                                 flags=cv.CV_HAAR_DO_CANNY_PRUNING,
-                                 min_size=(30, 30))
-    # draw rectangle around mouth
-    for (x, y, w, h), n in mobjs:
-        green = cv.Scalar(0, 128, 0)
-        cv.Rectangle(frame, (x, y), (x + w, y + h), green)
+def detect_features(frame):
+    """detect features based on loaded cascades and render the frame"""
+    detect_feature(frame, facecade, color=cv.Scalar(0, 0, 255))
+    detect_feature(frame, eyecade, color=cv.Scalar(255, 0, 0))
+    detect_feature(frame, mouthcade, color=cv.Scalar(0, 128, 0))
 
     # return the processed frame to display
     cv.ShowImage(name, frame)
@@ -72,8 +53,8 @@ def detect(frame):
 def loop():
     """main video loop to render frames
 
-    This loops until escape key is pressed, calling detect() on each frame to
-    look for faces. On breaking from the loop, the windows are destroyed.
+    This loops until escape key is pressed, calling detect_features on each
+    frame to look for faces.
     """
     while True:
         # escape key to quit
@@ -81,7 +62,7 @@ def loop():
             break
         # grab frame from camera and
         frame = cv.QueryFrame(source)
-        detect(frame)
+        detect_features(frame)
 
     # close GUI Window
     cv.DestroyAllWindows()
