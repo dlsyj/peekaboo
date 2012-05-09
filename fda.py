ead4cf5b1f84fac6a62f146f508133881096321e
Derek Hurley <dzhurley@gmail.com>
2012-05-08 21:24:42 -0700

    wip: refactoring and tweaking for performance and accuracy

--- fda.py
+++ fda.py
@@ -9,22 +9,15 @@ import cv
 name = "tests"
 source = cv.CaptureFromCAM(0)
 cv.NamedWindow(name)
+storage = cv.CreateMemStorage()
 
 # load all the necessary training sets
-facecade = cv.Load("data/haarcascade_frontalface_default.xml")
+facecade = cv.Load("data/haarcascade_frontalface_alt.xml")
 eyecade = cv.Load("data/haarcascade_eye.xml")
 mouthcade = cv.Load("data/haarcascade_mcs_mouth.xml")
 
-# consistent HaarDetectObjects parameters
-scale = 1.2
-flags = cv.CV_HAAR_DO_CANNY_PRUNING
-neighbors = 2
 
-# storage for opencv work
-storage = cv.CreateMemStorage()
-
-
-def detect_feature(frame, cade, color=255):
+def detect_feature(frame, cade, color=255, min_size=(60, 60)):
     """detect a single feature and draw a box around the matching area
 
     By using Canny Pruning, we disregard certain image regions to boost the
@@ -32,9 +25,11 @@ def detect_feature(frame, cade, color=255):
     affects performance as well.
     """
     # detect feature in image
-    objs = cv.HaarDetectObjects(frame, cade, storage, scale_factor=scale,
-                                min_neighbors=neighbors, flags=flags,
-                                min_size=(80, 80))
+    objs = cv.HaarDetectObjects(frame, cade, storage,
+                                scale_factor=1.2,
+                                min_neighbors=3,
+                                flags=cv.CV_HAAR_DO_CANNY_PRUNING,
+                                min_size=min_size)
     # draw rectangle around matched feature
     for (x, y, w, h), n in objs:
         cv.Rectangle(frame, (x, y), (x + w, y + h), color)
@@ -42,9 +37,15 @@ def detect_feature(frame, cade, color=255):
 
 def detect_features(frame):
     """detect features based on loaded cascades and render the frame"""
-    detect_feature(frame, facecade, color=cv.Scalar(0, 0, 255))
-    detect_feature(frame, eyecade, color=cv.Scalar(255, 0, 0))
-    detect_feature(frame, mouthcade, color=cv.Scalar(0, 128, 0))
+    # red, bigger
+    detect_feature(frame, facecade,
+                   color=cv.Scalar(0, 0, 255), min_size=(60, 60))
+    # blue, much smaller
+    detect_feature(frame, eyecade,
+                   color=cv.Scalar(255, 0, 0), min_size=(30, 30))
+    # green, still sort of small
+    detect_feature(frame, mouthcade,
+                   color=cv.Scalar(0, 128, 0), min_size=(30, 30))
 
     # return the processed frame to display
     cv.ShowImage(name, frame)
@@ -60,7 +61,7 @@ def loop():
         # escape key to quit
         if cv.WaitKey(15) == 27:
             break
-        # grab frame from camera and
+        # grab frame from camera and process
         frame = cv.QueryFrame(source)
         detect_features(frame)
 
