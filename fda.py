b6b52bd9570d989a3742ac189a70b1c1d785b823
Derek Hurley <dzhurley@gmail.com>
2012-06-01 16:54:27 -0700

    cleaned up some comments and naming conventions

--- fda.py
+++ fda.py
@@ -1,5 +1,10 @@
 """
-Face detection and augmentation script using OpenCV
+Derek Hurley
+6/6/12
+CS 510
+Term Project
+
+Facial Feature Detection and Augmentation
 """
 
 import cv
@@ -10,7 +15,7 @@ face_cade  = cv.Load("data/haarcascade_frontalface_alt.xml")
 eye_cade   = cv.Load("data/haarcascade_eye.xml")
 nose_cade  = cv.Load("data/haarcascade_mcs_nose.xml")
 
-# first image overlay
+# images to overlay on features
 moustache = cv.LoadImage("data/moustache.png")
 eyeball   = cv.LoadImage("data/eye.png")
 tophat    = cv.LoadImage("data/tophat.png")
@@ -19,7 +24,7 @@ tophat    = cv.LoadImage("data/tophat.png")
 cv.Flip(tophat, tophat, flipMode=0)
 
 # bools for which features to detect, set via number keys
-face, eye, nose = False, False, False
+face_on, eye_on, nose_on = False, False, False
 
 
 def overlay_image(frame, image, x, y, w, h):
@@ -28,28 +33,31 @@ def overlay_image(frame, image, x, y, w, h):
     This resizes the corresponding image to a matched feature, then loops
     through all of its pixels to superimpose the image on the frame
     """
+    # resize the image to fit the detected feature
     new_feature = cv.CreateImage((w, h), 8, 3)
     cv.Resize(image, new_feature, interpolation=cv.CV_INTER_AREA)
 
     # overlay the image on the frame
     for py in xrange(h):
         for px in xrange(w):
-            over = cv.Get2D(new_feature, py, px)
+            pixel = cv.Get2D(new_feature, py, px)
 
             # don't map the whitespace surrounding the image
-            if over != (255.0, 255.0, 255.0, 0.0):
-                # set the proper offsets on drawing the images
+            if pixel != (255.0, 255.0, 255.0, 0.0):
                 if image is tophat:
+                    # above feature
                     new_y = y - py
                 elif image is moustache:
+                    # bottom half of feature
                     new_y = (h / 2) + y + py
                 else:
+                    # over feature
                     new_y = y + py
                 new_x = x + px
 
                 # make sure the image is in the frame
                 if 0 < new_x < frame.width and 0 < new_y < frame.height:
-                    cv.Set2D(frame, new_y, new_x, over)
+                    cv.Set2D(frame, new_y, new_x, pixel)
 
 
 def detect_feature(frame, cade, min_size=(30, 30)):
@@ -77,11 +85,11 @@ def detect_feature(frame, cade, min_size=(30, 30)):
 
 def detect_features(frame):
     """detect features based on loaded cascades and render the frame"""
-    if face:
+    if face_on:
         detect_feature(frame, face_cade, min_size=(60, 60))
-    if eye:
+    if eye_on:
         detect_feature(frame, eye_cade)
-    if nose:
+    if nose_on:
         detect_feature(frame, nose_cade)
 
 
@@ -89,10 +97,11 @@ def loop():
     """main video loop to render frames
 
     This loops until escape key is pressed, calling detect_features on each
-    frame to look for faces.
+    frame to look for True features (based on keys 1-3).
     """
-    global face, eye, nose
+    global face_on, eye_on, nose_on
 
+    # set up window and camera
     win_name = "peekaboo"
     source = cv.CaptureFromCAM(0)
     cv.NamedWindow(win_name)
@@ -107,13 +116,13 @@ def loop():
 
         # keys 1-3 determine detected features
         if key == 49:
-            face = False if face else True
+            face_on = False if face_on else True
         if key == 50:
-            eye = False if eye else True
+            eye_on = False if eye_on else True
         if key == 51:
-            nose = False if nose else True
+            nose_on = False if nose_on else True
 
-        # get the frame, detect features, and show frame
+        # get the frame, detect selected features, and show frame
         frame = cv.QueryFrame(source)
         detect_features(frame)
         cv.ShowImage(win_name, frame)
